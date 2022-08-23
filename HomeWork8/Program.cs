612b4a37130da0c9f36cb69e01176356d2e3235b
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-08-23 14:50:21 +0300

    HomeWork8

---
+++ HomeWork8/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string message;
+        int repeat;
+        int numberOfReplay;
+
+        Console.Write("Введите сообщение: ");
+        message = Console.ReadLine();
+        Console.Write("Введите количество повторов: ");
+        numberOfReplay = Convert.ToInt32(Console.ReadLine());
+
+        for (repeat = 0; repeat < numberOfReplay; repeat++)
+        {
+            Console.WriteLine(repeat + message);
+        }
+    }
+}
